4e49d7495f5ebab0472aa731d5f7072e4b40422b
Harsh Kumar Suryan <89727538+HKSuryan@users.noreply.github.com>
2024-01-24 16:38:56 +0530

    Task Done

--- lfx_task.py
+++ lfx_task.py
@@ -1,10 +1,15 @@
 def prog(listOfInteger:list):
+  for ele in listOfInteger:
+    if not isinstance(ele, int):
+       return"Error: List is not a list of integers"
+
   if isinstance(listOfInteger,str) or len(listOfInteger) == 0:
-    print("Error: List is not a list of integers")
-    return
+    return"Error: List is not a list of integers"
+
   
   if len(listOfInteger)%10 != 0:
-    print("Error: List is not a multiple of 10 in length")
+    return"Error: List is not a multiple of 10 in length"
+
   else:
     return [listOfInteger[i] for i in range(len(listOfInteger)) if (i+1)%2 != 0 and (i+1)%3 != 0]
 
@@ -25,3 +30,6 @@ print(prog([1,2,3,4,5,6,7,"Hi",9,10,11,12,13,14,15,"Hello",17,18,19,20]))
 #List with length 9 (boundary of not a multiple of 10):
 print(prog([1,2,3,4,5,6,7,8,9]))
 
+
+#List with negative integers
+print(prog([1,2,3,4,5,6,7,8,9,-10,-11,-12,-13,-14,-15,-16,-17,-18,-19,-20]))
